091b551964b94a8750e8491a509800c2a7ac4160
Jonathan Peppers <jonathan.peppers@gmail.com>
2013-09-05 14:35:09 -0500

    Added test project

---
+++ PickANick/PickANick.Tests/NickViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PickANick.Core.ViewModels;
+using PickANick.Core;
+
+namespace PickANick.Tests
+{
+    [TestClass]
+    public class NickViewModelTests
+    {
+        private NickViewModel _nickViewModel;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            ServiceContainer.Register<INickServer>(() => new FakeNickServer());
+
+            _nickViewModel = new NickViewModel();
+        }
+
+        [TestMethod]
+        public void GetNicks()
+        {
+            var task = _nickViewModel.GetNicks();
+
+            task.Wait();
+
+            Assert.IsNotNull(_nickViewModel.Nicks);
+            Assert.AreNotEqual(0, _nickViewModel.Nicks.Length);
+        }
+    }
+}
